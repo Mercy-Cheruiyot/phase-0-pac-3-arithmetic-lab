a4216f11059d50c5f463c6c63dadbc5f86f74cce
Mercy <mercycheruiyot2013@gmail.com>
2022-08-09 12:02:32 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b){
+    return a + b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+
+function increment(n){
+    return ++n ;
+
+}
+increment(n);
+function decrement(n){
+    return --n ;
+}
+
+function makeInt(n){
+  return parseInt(n,10);
+}
+makeInt;
+
+function  preserveDecimal(n){
+return parseFloat(n);
+}
+preserveDecimal;
